338a96d7fb136d4a71cdf7d9f05f94e983469b5f
Technology Gardening <33565151+TechnologyGardening@users.noreply.github.com>
2024-07-23 11:02:50 +0530

    .

--- lib/stopwatch.dart
+++ lib/stopwatch.dart
@@ -9,17 +9,61 @@ class StopWatch extends StatefulWidget {
 }
 
 class _StopWatchState extends State<StopWatch> {
+  int seconds = 0;
+  late Timer timer;
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(const Duration(seconds: 1), _ontick);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Stop Watch"),
         ),
-        body: Center(
-          child: Text(
-            '0 Seconds',
-            style: Theme.of(context).textTheme.headlineSmall,
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                '$seconds Seconds',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: null,
+                  style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.black),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.amber)),
+                  child: const Text("Start"),
+                )
+              ],
+            )
+          ],
         ));
+  }
+
+  void _ontick(Timer timer) {
+    if (mounted) {
+      setState(() {
+        ++seconds;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
   }
 }
